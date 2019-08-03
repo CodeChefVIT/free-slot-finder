2d27a0d06e5b2b4933cc2b5d7834ae11805b8eb9
RajatSablok <rajat.main06@gmail.com>
2019-08-04 02:20:08 +0530

    changed template engine to ejs

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const {PythonShell} = require("python-shell");
 var upload = multer({ dest: 'uploads/' })
 const app = express();
 app.use(bodyParser.json());
+const fs = require('fs');
 
 // Set static folder
 app.use(express.static('./public'));
 
+// EJS
+app.set('view engine', 'ejs');
 
 // Check File Type
 function checkFileType(file, cb){
@@ -27,52 +30,55 @@ function checkFileType(file, cb){
   }
 }
 
-// EJS
-// app.set('view engine', 'html');
-
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/upload", upload.single("file"), function(req, res) {
-  let filepath = "uploads/" + req.file.filename
-  let newfilepath = "uploads/" + req.body.text
-  console.log(req.file)
-  console.log(req.body.text)
-  //fs.rename(filepath, newfilepath, (err) => {
-
-  //})
-
-PythonShell.run('script.py', {args:[filepath]}, function (err) {
-  if (err) throw err;
-  console.log('Ran python file');
-  res.sendFile(__dirname + "/public/upload.html");
+var storage = multer.diskStorage({
+	destination: function(req, file, callback) {
+		callback(null, './public/uploads')
+	},
+	filename: function(req, file, callback,) {
+    // console.log(file)
+		callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+	}
 })
+
+app.post('/upload', function(req, res) {
+	var upload = multer({
+		storage: storage
+	}).single('file')
+	upload(req, res, function(err) {
+      res.render('upload', { fs: fs });   
+
+      console.log(req.file)
+      console.log(req.body.text)    
+      let filepath = __dirname + "/public/uploads/" + req.file.filename
+      let newfilepath = __dirname + "/public/uploads/" + req.body.text 
+      fs.rename(filepath, newfilepath, (err) => {
+        if (err) throw err;
+        console.log('Rename complete!');
 });
-
-// app.post('/upload', (req, res) => {
-//   upload(req, res, (err) => {
-//     if(err){
-//       res.render('index', {
-//         msg: err
-//       });
-//     } else {
-//       if(req.file == undefined){
-//         res.render('index', {
-//           msg: 'Error: No File Selected!'
-//         });
-//       } else {
-//         res.render('index', {
-//           msg: 'File Uploaded!',
-//           file: `uploads/${req.file.filename}`
-//         });
-//       }
-//     }
-//   });
-// });
-
+	})
+})
 
 
 const PORT = process.env.PORt || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+// app.post("/uploads", upload.single('file'), function(req, res) {
+//   let filepath = "/public/uploads/" + req.file.filename
+//   let newfilepath = "/public/uploads/" + req.body.text
+//   console.log(req.file)
+//   console.log(req.body.text)
+//   //fs.rename(filepath, newfilepath, (err) => {
+
+//   //})
+// // PythonShell.run('script.py', {args:[filepath]}, function (err) {
+// //   if (err) throw err;
+// //   console.log('Ran python file');
+// //   res.sendFile(__dirname + "/public/upload.html");
+// // })
+// });
