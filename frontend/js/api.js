63be2244de2013fb37ff81b6ea0d590a9cb5d9b9
Shashwat Gupta <shashwatsatna@gmail.com>
2020-11-09 01:56:43 +0530

    add login

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,23 +1,45 @@
-get = () => {
+get = (folder, token, callback) => {
+  var requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    redirect: "follow",
+  };
 
+  fetch(`https://free-slot-finder-app.herokuapp.com/${folder}`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      res = JSON.parse(result).teams;
+      console.log(res);
+
+      callback();
+    })
+    .catch((error) => console.log("error", error));
 };
 
-post = (folder,raw) => {
-  
+post = (folder, raw, redir, login, token, callback) => {
   var requestOptions = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-type' : 'application/json'
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(raw) ,
-    redirect: 'follow'
+
+    body: JSON.stringify(raw),
   };
-  console.log(`https://free-slot-finder-app.herokuapp.com/${folder}`)
   fetch(`https://free-slot-finder-app.herokuapp.com/${folder}`, requestOptions)
-    .then(response => response.text())
-    .then(result => {
-      console.log(result)
-      window.location.href = "dashboard.html";
+    .then((response) => {
+      console.log(response.status);
+      if (response.status >= 200 && response.status < 300 && redir) {
+        window.location.href = "dashboard.html";
+      }
+      return response.text();
     })
-    .catch(error => console.log('error', error));
+    .then((result) => {
+      if (login) {
+        localStorage.setItem("user", result);
+      }
+    })
+    .catch((error) => console.log("error", error));
 };
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,27 +1,29 @@
 signinEmail = document.querySelector("#signin-email").value;
 signinPass = document.querySelector("#signin-pass").value;
 
-
-loginBtn = document.querySelector("#login-btn").addEventListener("click", () => {
-  signinEmail = document.querySelector("#signin-email").value;
-  signinPass = document.querySelector("#signin-pass").value;
-  let data = {
-    "email": signinEmail,
-    "password": signinPass
-};
-console.log(data);
-post("user/login",data);
-} );
-signupBtn = document.querySelector("#signup-btn").addEventListener("click",() => {
-  signupEmail = document.querySelector("#signup-email").value;
-  signupPass = document.querySelector("#signup-pass").value;
-  signupName = document.querySelector(".signup-name").value;
-  let data = {
-        "name": signupName,
-        "email": signupEmail,
-        "password": signupPass
+loginBtn = document
+  .querySelector("#login-btn")
+  .addEventListener("click", () => {
+    signinEmail = document.querySelector("#signin-email").value;
+    signinPass = document.querySelector("#signin-pass").value;
+    let data = {
+      email: signinEmail,
+      password: signinPass,
     };
-    console.log(data);
-  post("user/signup",data)
-});
+    post("user/login", data, true, true,null);
+  });
+signupBtn = document
+  .querySelector("#signup-btn")
+  .addEventListener("click", () => {
+    signupEmail = document.querySelector("#signup-email").value;
+    signupPass = document.querySelector("#signup-pass").value;
+    signupName = document.querySelector(".signup-name").value;
+    let data = {
+      name: signupName,
+      email: signupEmail,
+      password: signupPass,
+    };
+    post("user/signup", data, true, true,null);
+  });
+
 
